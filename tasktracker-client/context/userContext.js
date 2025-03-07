8b3fefb0897251ec25c188e3ff03765405e5b3e7
dxdzdy <dxdzdydxdzdy@gmail.com>
2025-03-07 20:19:16 +0300

    add url

--- tasktracker-client/context/userContext.js
+++ tasktracker-client/context/userContext.js
@@ -9,7 +9,7 @@ const UserContext = React.createContext();
 axios.defaults.withCredentials = true;
 
 export const UserContextProvider = ({ children }) => {
-  const serverUrl = "http://localhost:8000";
+  const serverUrl = "https://tasktracker-v4gj.onrender.com";
 
   const router = useRouter();
 
